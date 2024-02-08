7866472617cf3d5eab98b3f3c07577c4fac882f5
Kundan <kundanpatel2211@gmail.com>
2024-02-08 11:36:23 +0530

    Reduce code client section

--- src/components/companyLogo/index.jsx
+++ src/components/companyLogo/index.jsx
@@ -46,127 +46,159 @@ const logo = [
   {
     id: 8,
     c_name: 'Hindalco Industries logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243272/DTS-Aircon/Clients/8._Hindalco-Industries-logo_fth7es.png',
   },
   {
     id: 9,
     c_name: 'FEDERAL MOGUL logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243279/DTS-Aircon/Clients/9._FEDERAL_MOGUL_logo_yugdru.png',
   },
   {
     id: 10,
     c_name: 'Punj Lloyd group logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243282/DTS-Aircon/Clients/10._Punj_Lloyd_group_logo_eujn4w.png',
   },
   {
     id: 11,
     c_name: 'Competent automobiles logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243285/DTS-Aircon/Clients/11._Competent_automobiles_logo_ewtnpy.png',
   },
   {
     id: 12,
     c_name: 'iNDOSOLAR logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243288/DTS-Aircon/Clients/12._iNDOSOLAR_logo_hxph3y.png',
   },
   {
     id: 13,
     c_name: 'Uop - a honeywell company logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243292/DTS-Aircon/Clients/13._Uop_-_a_honeywell_company_logo_alwtuh.png',
   },
   {
     id: 14,
     c_name: 'EICHER logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243295/DTS-Aircon/Clients/14._EICHER_logo_rfvrxt.png',
   },
   {
     id: 15,
     c_name: 'Bajaj logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243299/DTS-Aircon/Clients/15._Bajaj_logo_ekaknv.png',
   },
   {
     id: 16,
     c_name: 'CATERPILLAR logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243302/DTS-Aircon/Clients/16._CATERPILLAR_logo_s8qmzf.png',
   },
   {
     id: 17,
     c_name: 'Lufthansa logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243305/DTS-Aircon/Clients/17._Lufthansa_logo_hu2zd0.png',
   },
   {
     id: 18,
     c_name: 'Hero Honda logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243309/DTS-Aircon/Clients/18._Hero_Honda_logo_kgjrkg.png',
   },
   {
     id: 19,
     c_name: 'Ashoka l logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243312/DTS-Aircon/Clients/19._Ashoka_l_logo_ubjrra.png',
   },
   {
     id: 20,
     c_name: 'CAE logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243361/DTS-Aircon/Clients/21._CAE_logo_tuqzfi.png',
   },
   {
     id: 21,
     c_name: 'GODREJ logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243318/DTS-Aircon/Clients/22._GODREJ_logo_k6vdwg.png',
   },
   {
     id: 22,
     c_name: 'HP-invent logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243321/DTS-Aircon/Clients/23._HP-invent_logo_tvlqnx.png',
   },
   {
     id: 23,
     c_name: 'tata logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243362/DTS-Aircon/Clients/24._tata_logo_qlka9c.png',
   },
   {
     id: 24,
     c_name: 'Satyamev Jayte logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243363/DTS-Aircon/Clients/25._Satyamev_Jayte_logo_ohwrjn.png',
   },
   {
     id: 25,
     c_name: 'Infosys logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243363/DTS-Aircon/Clients/26._Infosys_logo_ge3gau.png',
   },
   {
     id: 26,
     c_name: 'asianpaints logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243364/DTS-Aircon/Clients/27._asianpaints_logo_qxxpmc.png',
   },
   {
     id: 27,
     c_name: 'ABB logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243364/DTS-Aircon/Clients/28._ABB_logo_vmdvk0.png',
   },
   {
     id: 28,
     c_name: 'Coca-cola logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243364/DTS-Aircon/Clients/29._Coca-cola_logo_smjqwu.png',
   },
   {
     id: 29,
     c_name: 'GM Motors',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243364/DTS-Aircon/Clients/30._GM_Motors_cxhytm.png',
   },
   {
     id: 30,
     c_name: 'TTK logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243365/DTS-Aircon/Clients/31._TTK_logo_twoiqu.png',
   },
   {
     id: 31,
     c_name: 'GD Housing logo',
-    c_img: '',
+    c_img:
+      'https://res.cloudinary.com/ddffgfse1/image/upload/v1707243365/DTS-Aircon/Clients/32._GD_Housing_logo_vfdlae.png',
   },
 ];
 
 function CompanyLogo() {
-  return <div>CompanyLogo</div>;
+  return (
+    <>
+      {logo.map(x => (
+        <div className="hexagon">
+          <img src={x.c_img} alt={x.c_name} data-aos="fade-up" />
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default CompanyLogo;

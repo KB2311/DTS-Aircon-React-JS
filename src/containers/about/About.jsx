0e12621c41e08680bb3f39b2797d20e63552e3dc
Kundan <kundanpatel2211@gmail.com>
2024-02-10 17:36:32 +0530

    Add

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function About() {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h2 className="section_title">
+        About <span>Us</span>
+      </h2>
+    </section>
+  );
 }
 
 export default About;
--- src/containers/clients/Client.jsx
+++ src/containers/clients/Client.jsx
@@ -7,7 +7,9 @@ function Client() {
       <div className="mx-auto flex max-w-7xl flex-col items-center">
         <div className="border-b-[3px] border-sky-500" data-aos="fade-up">
           <p className="headline_of_section text-2xl font-semibold">
-            Major Clients
+            <h2 className="section_title">
+              Major <span>Clients</span>
+            </h2>
           </p>
         </div>
         <div className="mt-11 flex h-3/5 flex-wrap justify-center gap-2">

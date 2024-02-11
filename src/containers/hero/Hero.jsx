22516bda36c3a73785eda7f3d105127e55860c52
KB2311 <kunjbiharihirani@gmail.com>
2024-02-11 14:15:42 +0530

    hero page responsive done kb

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -5,12 +5,14 @@ function Hero() {
   return (
     <div className="h-screen pt-14" id="hero">
       <div className="flex h-full items-center justify-center text-center">
-        <div className=" bg-black bg-opacity-65 p-9 font-bold lg:rounded-full ">
-          <h2 className=" pt-5 text-5xl text-white">
+        <div className=" bg-black bg-opacity-65 py-3 font-bold xsm:rounded-3xl xsm:p-9 lg:rounded-full ">
+          <h2 className=" px-1 pt-5 text-[7vw] text-white xsm:text-3xl lg:text-5xl">
             ❝ <span className="text-secondary">Building - The Revolution</span>{' '}
             ❞
           </h2>
-          <h2 className="py-10 text-3xl">MEP & HVAC Contracting Services</h2>
+          <h2 className="py-10 text-[5vw] xsm:text-xl lg:text-3xl">
+            MEP & HVAC Contracting Services
+          </h2>
           <a
             href="https://ik.imagekit.io/b8au2iwe2/DTS%20Aircon%20Images/DTS%20Brochure%20Final.pdf?updatedAt=1707572697810"
             download="DTS Aircon Brochure"
@@ -18,7 +20,7 @@ function Hero() {
             rel="noreferrer"
           >
             <button
-              className="rounded-full px-4 py-2 text-xl uppercase text-white ring-2 ring-white"
+              className="rounded-full px-4 py-2 text-base uppercase text-white ring-2 ring-white xsm:text-lg lg:text-xl"
               type="button"
               id="get_Brochure"
             >
